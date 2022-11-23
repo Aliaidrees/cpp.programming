710dad7a1cc05cd47aa2b3fd66d9900ae1673884
Aliaidrees <118732619+Aliaidrees@users.noreply.github.com>
2022-11-23 00:51:13 -0800

    Create while loop.cpp

---
+++ while loop.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int valueOne;
+  int valueTwo;
+  cout<<"enter the valueOne"<<endl;
+  cin>>valueOne;
+  cout<<"enter a valueTwo";
+  cin>>valueTwo;
+  cout<<"the result is"<<valueOne+valueTwo;	
+}
