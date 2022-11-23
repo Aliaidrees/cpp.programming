8d69eea38b9704927ca4f8da5f60c27129332f23
Aliaidrees <118732619+Aliaidrees@users.noreply.github.com>
2022-11-23 01:04:26 -0800

    Create while program.cpp

---
+++ while program.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int i=1;
+  while(i==1)
+  {
+  	cout<<"our program is running good"<<endl;
+  	cout<<"enter 1key to continue and press 0 for terminate";
+  	cin>>i;
+	  }	
+}
