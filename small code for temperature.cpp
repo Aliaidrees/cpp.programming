707779fc3f7abc0d62befd3d8f9dff37b59a7f59
Aliaidrees <118732619+Aliaidrees@users.noreply.github.com>
2022-11-24 04:17:15 -0800

    Create small code for temperature.cpp

---
+++ small code for temperature.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number;
+	cout<<"please enter your choice\n 1 is for fah to cel\n 2 is cel to fah\n";
+	cin>>number;
+	if(number==1)
+	{
+		int fah;
+		cout<<"enter a temperature in fah";
+		cin>>fah;
+		int cel=(fah-32)*5/9;
+		cout<<"equivalent temperature in celcius is"<<cel<<endl;
+		
+	}
+	else if(number==2)
+	{
+		int cel;
+		cout<<"enter a temperature in cel";
+		cin>>cel;
+		int fah=cel*(5/9)+32;
+		cout<<"equivalent temperature in fah is"<<fah<<endl;
+		
+	}
+	else
+	{
+		cout<<"invalid choice";
+	}
+}
