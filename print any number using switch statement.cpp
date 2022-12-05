3b95d8d5ba351da47d5a770cbf2b9f9a830b796b
Aliaidrees <118732619+Aliaidrees@users.noreply.github.com>
2022-12-05 06:54:01 -0800

    Create print any number using switch statement.cpp

---
+++ print any number using switch statement.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n;
+	cout<<"enter a number";
+	cin>>n;
+	switch(n)
+	{
+		case 1:
+			cout<<"you pressesd 1";
+			break;
+		case 2:
+			cout<<"you pressed 2";
+			break;
+		case 3:
+		   cout<<"you pressed 3";
+		default:
+			cout<<"wrong entry";
+	}
+	
+}
